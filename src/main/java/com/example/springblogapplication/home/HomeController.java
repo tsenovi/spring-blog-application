f44e4605d71e5568e478156294950e06cf1bc300
tsenovi <vandertsen@gmail.com>
2023-01-24 02:00:19 +0200

    Field-based changed to constructor-based DI

--- src/main/java/com/example/springblogapplication/home/HomeController.java
+++ src/main/java/com/example/springblogapplication/home/HomeController.java
@@ -3,16 +3,17 @@ package com.example.springblogapplication.home;
 import com.example.springblogapplication.post.Post;
 import com.example.springblogapplication.post.PostService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class HomeController {
 
-  @Autowired
-  private PostService postService;
+  private final PostService postService;
 
   @GetMapping("/")
   public String home(Model model) {
--- src/main/java/com/example/springblogapplication/post/PostController.java
+++ src/main/java/com/example/springblogapplication/post/PostController.java
@@ -1,18 +1,18 @@
 package com.example.springblogapplication.post;
 
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PostController {
 
-  @Autowired
-  private PostService postService;
+  private final PostService postService;
 
   @GetMapping("/posts/{id}")
   public String getPost(@PathVariable Long id, Model model) {
